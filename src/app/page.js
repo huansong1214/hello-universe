f3d423ebc3bee37843202c120a74209beaff4e72
huansong1214 <huansong1214@gmail.com>
2025-06-24 16:49:18 -0400

    Add apod.module.css

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,23 @@
+import styles from './apod.module.css';
+
 export default async function Apod() {
   const response = await fetch('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY');
   const apod = await response.json();
 
   return (
-    <main>
-      <h1>Astronomy Picture of the Day</h1>
-      <p>
-        <img src={apod.url} />
-      </p>
-      <h2>{apod.date}</h2>
-      <p>{apod.title}</p>
-      <p>{apod.explanation}</p>
-    </main>
+    <>
+      <h1 className={styles.heading}>Astronomy Picture of the Day</h1>
+      <main className={styles.grid}>
+        <div className={styles.imageContainer}>
+          <img src={apod.url} alt={apod.title}/>
+        </div>
+        <div className={styles.text}>
+          <h2>{apod.date}</h2>
+          <h3 className={styles.subheading}>{apod.title}</h3>
+          <p>{apod.explanation}</p>
+        </div>
+      </main>
+    </>
+
   );
 }
