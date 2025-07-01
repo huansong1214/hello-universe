ad32861513f421d4ab247c2cd7c46f9013676eb5
huansong1214 <huansong1214@gmail.com>
2025-07-01 14:49:53 -0400

    refactor: use named function in forwardRef

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -3,8 +3,8 @@ import React from "react";
 export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-    ({ type = "text", ...props }, ref) => {
-    return <input type={type} ref={ref} {...props} />;
+    function Input({ type = "text", ...props }, ref) {
+        return <input type={type} ref={ref} {...props} />
     }
 );
 
