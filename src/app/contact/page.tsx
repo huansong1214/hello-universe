520c1990dfd73f6cfcb5fa83c65f49d906b1f364
huansong1214 <huansong1214@gmail.com>
2025-07-02 11:48:22 -0400

    chore: test rendering ContactForm in contact page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,10 @@
+import ContactForm from "./ContactForm";
+
+export default function ContactPage() {
+    return (
+        <main>
+            <h1>Contact Us</h1>
+            <ContactForm />
+        </main>
+    );
+}
