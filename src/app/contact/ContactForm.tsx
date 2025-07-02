f8544ca3eea5a07c7c23534c987fa79998785206
huansong1214 <huansong1214@gmail.com>
2025-07-02 19:41:24 -0400

    style: add ContactForm and contact page CSS

--- src/app/contact/ContactForm.tsx
+++ src/app/contact/ContactForm.tsx
@@ -14,6 +14,8 @@ import {
 } from "@/components/ui/form";
 import { useState } from "react";
 
+import styles from "./ContactForm.module.css";
+
 // schema
 const contactSchema = z.object({
     name: z.string().min(1, "Name is required"),
@@ -39,77 +41,84 @@ export default function ContactForm() {
 
     return (
         <>
-            <Form schema={contactSchema} onSubmit={onSubmit}>
-                {/* name */}
-                <FormField name="name">
-                    {({ value = "", onChange, onBlur, ref }) => (
-                        <FormItem>
-                            <FormLabel htmlFor="name">Name</FormLabel>
-                            <FormControl>
-                                <Input
-                                    id="name"
-                                    value={value}
-                                    onChange={onChange}
-                                    onBlur={onBlur}
-                                    ref={ref}
-                                    placeholder="Your name here"
-                                />
-                            </FormControl>
-                            <FormMessage name="name" />
-                        </FormItem>
-                    )}
-                </FormField>
+            <div className={styles.formContainer}>
+                <Form schema={contactSchema} onSubmit={onSubmit}>
+                    {/* name */}
+                    <div className={styles.fieldsContainer}>
+                        <FormField name="name">
+                            {({ value = "", onChange, onBlur, ref }) => (
+                                <FormItem>
+                                    <FormLabel htmlFor="name">Name</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            id="name"
+                                            value={value}
+                                            onChange={onChange}
+                                            onBlur={onBlur}
+                                            ref={ref}
+                                            placeholder="Your name here"
+                                            className={styles.input}
+                                        />
+                                    </FormControl>
+                                    <FormMessage name="name" />
+                                </FormItem>
+                            )}
+                        </FormField>
+                        
+                        {/* email */}
+                        <FormField name="email">
+                            {({ value = "", onChange, onBlur, ref }) => (
+                                <FormItem>
+                                    <FormLabel htmlFor="email">Email</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            id="email"
+                                            type="email"
+                                            value={value}
+                                            onChange={onChange}
+                                            onBlur={onBlur}
+                                            ref={ref}
+                                            placeholder="Your email here"
+                                            className={styles.input}
+                                        />
+                                    </FormControl>
+                                    <FormMessage name="email" />
+                                </FormItem>
+                            )}
+                        </FormField>
+
+                        {/* message */}
+                        <FormField name="message">
+                            {({ value = "", onChange, onBlur, ref }) => (
+                                <FormItem>
+                                    <FormLabel htmlFor="message">Message</FormLabel>
+                                    <FormControl>
+                                        <Textarea
+                                            id="message"
+                                            value={value}
+                                            onChange={onChange}
+                                            onBlur={onBlur}
+                                            ref={ref}
+                                            placeholder="Any feedback is appreciated..."
+                                            rows={5}
+                                            className={styles.textarea}
+                                        />
+                                    </FormControl>
+                                    <FormMessage name="message" />
+                                </FormItem>
+                            )}
+                        </FormField>
+
+                        {/* submit */}
+                        <Button type="submit" className={styles.button}>Send Message</Button>
                 
-                {/* email */}
-                <FormField name="email">
-                    {({ value = "", onChange, onBlur, ref }) => (
-                        <FormItem>
-                            <FormLabel htmlFor="email">Email</FormLabel>
-                            <FormControl>
-                                <Input
-                                    id="email"
-                                    type="email"
-                                    value={value}
-                                    onChange={onChange}
-                                    onBlur={onBlur}
-                                    ref={ref}
-                                    placeholder="Your email here"
-                                />
-                            </FormControl>
-                            <FormMessage name="email" />
-                        </FormItem>
-                    )}
-                </FormField>
-
-                {/* Message */}
-                <FormField name="message">
-                    {({ value = "", onChange, onBlur, ref }) => (
-                        <FormItem>
-                            <FormLabel htmlFor="message">Message</FormLabel>
-                            <FormControl>
-                                <Textarea
-                                    id="message"
-                                    value={value}
-                                    onChange={onChange}
-                                    onBlur={onBlur}
-                                    ref={ref}
-                                    placeholder="Any feedback is appreciated..."
-                                    rows={5}
-                                />
-                            </FormControl>
-                            <FormMessage name="message" />
-                        </FormItem>
-                    )}
-                </FormField>
-
-                {/* submit */}
-                <Button type="submit">Send Message</Button>
-            </Form>
-
-            {/* feedback message */}
-            {success && (
-                <p>Message sent successfully</p>
-            )}
+                        {/* feedback message */}
+                        {success && (
+                            <p>Message sent successfully</p>
+                        )}
+                    </div>
+                </Form>
+            </div>
         </>
     );
 }
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,9 +1,11 @@
 import ContactForm from "./ContactForm";
 
+import styles from "./contact.module.css"
+
 export default function ContactPage() {
     return (
         <main>
-            <h1>Contact Us</h1>
+            <h1 className={styles.heading}>Contact Us</h1>
             <ContactForm />
         </main>
     );
