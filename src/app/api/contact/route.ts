14c1fd14e93a7d8207d2d253859315712b5aeb3e
huansong1214 <huansong1214@gmail.com>
2025-07-04 17:56:36 -0400

    refactor: rename and relocate email template

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,4 +1,4 @@
-import { EmailTemplate } from '@/components/email-template';
+import { EmailTemplate } from 'emails/ContactEmail';
 import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
