a50c3b53674cdfd62f81d0430b3e33ee00b20bd1
huansong1214 <huansong1214@gmail.com>
2025-07-05 16:09:03 -0400

    refactor: rename apiKey variable to NASA_API_KEY for clarity

--- src/app/apod/page.tsx
+++ src/app/apod/page.tsx
@@ -10,10 +10,10 @@ interface ApodData {
 }
 
 export default async function Apod() {
-  const apiKey = process.env.NASA_API_KEY;
+  const NASA_API_KEY = process.env.NASA_API_KEY;
 
   const response = await fetch(
-    `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`,
+    `https://api.nasa.gov/planetary/apod?api_key=${NASA_API_KEY}`,
   );
 
   if (!response.ok) {
