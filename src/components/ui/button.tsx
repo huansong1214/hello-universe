28045f510db9e089cf6a9078f39d232afe4b03a5
huansong1214 <huansong1214@gmail.com>
2025-07-05 14:19:14 -0400

    feat: add default styles and clsx to button component

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,9 +1,18 @@
 import React, { ButtonHTMLAttributes } from 'react';
+import clsx from 'clsx';
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
 
-function Button(props: ButtonProps) {
-  return <button {...props} />;
+function Button({ className, ...props }: ButtonProps) {
+  return (
+    <button 
+      className={clsx(
+        'bg-[#c693c6] text-[var(--background)] rounded-md px-4 py-2 my-2 mx-auto hover:bg-[#aa7eaa] hover:cursor-pointer',
+        className
+      )}
+      {...props} 
+    />
+  );
 }
 
 export { Button };
