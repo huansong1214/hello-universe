48503ad1b5f1189c540c83a18917b7ffa555f620
huansong1214 <huansong1214@gmail.com>
2025-07-16 01:01:52 -0400

    feat: support 'other' media type in modal and add link to NASA APOD page

--- src/components/ApodCalendar/Modal.tsx
+++ src/components/ApodCalendar/Modal.tsx
@@ -16,22 +16,39 @@ interface ModalProps {
 const Modal: React.FC<ModalProps> = ({ isOpen, onClose, apod }) => {
   if (!isOpen) return null;
 
+  const formattedDate = apod.date.replace(/-/g, '').slice(2); // 2025-05-18 => 250518
+  const nasaUrl = `https://apod.nasa.gov/apod/ap${formattedDate}.html`;
+
+  const renderMediaContent = () => {
+    if (apod.media_type === 'image') {
+      return <img src={apod.url} alt={apod.title} />;
+    } else if (apod.media_type === 'video') {
+      return (
+        <iframe
+          src={apod.url}
+          title={apod.title}
+          allowFullScreen
+          aria-label={apod.title}
+        />
+      );
+    } else if (apod.media_type === 'other') {
+      return (
+        <p className={styles.mediaOther}>
+          Watch video on <a href={nasaUrl} target='_blank' rel='noopener noreferrer' className={styles.nasaUrl}>NASA APOD page</a>
+        </p>
+      );
+    } else {
+      return <p className={styles.error}>Sorry, no media available for this content.</p>;
+    }
+  };
+
   return (
     <div className={styles.modalOverlay} onClick={onClose}>
       <div className={styles.modalContent} onClick={(event) => event.stopPropagation()}>
         <button className={styles.closeButton} onClick={onClose}>X</button>
         <div className={styles.grid}>
           <div className={styles.imageContainer}>
-            {apod.media_type === 'image' ? (
-              <img src={apod.url} alt={apod.title} />
-            ) : (
-              <iframe
-                src={apod.url}
-                title={apod.title}
-                allow="fullscreen"
-                allowFullScreen
-              />
-            )}
+            {renderMediaContent()}
           </div>
           <div className={styles.textContainer}>
             <h2>{apod.date}</h2>
